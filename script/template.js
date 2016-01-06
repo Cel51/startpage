b26992b6b6647ed1d46d3468e3cd213c79eff124
Célien Heritier <celien.heritier@gmail.com>
2016-01-06 13:33:10 +0100

    Update of the background and left-pannel
    Changing loading to greetings, added canvas on background with line
    drawn.

--- script/template.js
+++ script/template.js
@@ -1,10 +1,17 @@
 var load;
 var tlLoading;
+var tlHideLoading;
 
 $(document).ready(function (){
   initElements();
   initTimeLines();
-  loading(1);
+  tlLoading.play();
+  runClock();
+
+  setTimeout(function() {
+    tlLoading.pause();
+    tlHideLoading.play();
+  },1390);
 });
 
 function initElements() {
@@ -18,13 +25,53 @@ function initTimeLines() {
   .from($(".s3"),.6,{rotation: "-=180"},"#1")
   .from($(".s4"),.7,{rotation: "-=180"},"#1")
   .pause();
+
+  tlHideLoading = new TimelineMax()
+  .to($(".squares"),.2,{autoAlpha: 0})
+  .to($(".squares"),.2,{height: 0, margin: 0})
+  .from($("#greetings-board"),.4,{autoAlpha: 0, marginTop: "-20"},"#2")
+  .staggerFrom($("#greetings-board p"),.1,{autoAlpha: 0, marginLeft: "-20"},0.1)
+  .from($("#network-board"),.4,{autoAlpha: 0, marginTop: "-20"})
+
+  .pause();
 }
-function loading(mod) {
-  if (mod == 1) {
-    load.show();
-    tlLoading.play();
-  } else {
-    load.hide();
-    tlLoading.pause();
-  }
+function runClock() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+    dd='0'+dd
+    }
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    m = checkTime(m);
+    s = checkTime(s);
+
+    $(".time-hours").html(h);
+    $(".time-minutes").html(m);
+    $(".time-seconds").html(s);
+    $(".date-day").html(dd);
+    $(".date-month").html(mm);
+    $(".date-year").html(yyyy);
+
+    if(h<12){
+      $(".greetings-title").html("Good Morning");
+    } else if (h>=12 && h<19){
+      $(".greetings-title").html("Good Afternoon");
+    } else {
+      $(".greetings-title").html("Good Evening");
+    }
+
+    var t = setTimeout(runClock, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
