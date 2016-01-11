b61847b6f980836781871b1cf8c6199a58063816
Célien Heritier <celien.heritier@gmail.com>
2016-01-11 14:21:00 +0100

    Added the early version of the command line interface

---
+++ script/terminal.js
@@ -0,0 +1,20 @@
+jQuery(function($, undefined) {
+    $('#terminal').terminal(function(command, term) {
+        if (command !== '') {
+            try {
+                var result = window.eval(command);
+                if (result !== undefined) {
+                    term.echo(new String(result));
+                }
+            } catch(e) {
+                term.error(new String(e));
+            }
+        } else {
+           term.echo('');
+        }
+    }, {
+        greetings: 'Hello ' + username,
+        name: 'js_demo',
+        height: 300,
+        prompt: 'js> '});
+});
